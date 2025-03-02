c797fe9d4ded00249a6a04a906e050a5bdad21f0
Nehil Danis <nehild@outlook.com>
2025-03-02 16:07:48 +0100

    feat: added the Server struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+
+    fn new(addr: String) -> Self{ // new is an associated function
+        Server {
+            addr
+        }
+    }
+
+    fn run(self) {
+        println!("Listening on {}", self.addr);
+    }
+
 }
